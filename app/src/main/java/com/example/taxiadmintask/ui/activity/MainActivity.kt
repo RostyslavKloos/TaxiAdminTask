ce923ed11c964097c6697e683a36b2dfcd8fc3df
RostyslavKloos <rotsislavkloos@gmail.com>
2020-10-04 12:29:39 +0300

    problem with show postResponse on Fragment

--- app/src/main/java/com/example/taxiadmintask/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/taxiadmintask/ui/activity/MainActivity.kt
@@ -2,18 +2,20 @@ package com.example.taxiadmintask
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.example.taxiadmintask.data.model.Types
 import com.example.taxiadmintask.ui.adapter.SectionsPagerAdapter
 import com.example.taxiadmintask.ui.viewModel.PageViewModel
+import com.example.taxiadmintask.utils.SessionManager
 import com.example.weatherapp.utils.Resource
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_main.*
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pageViewModel: PageViewModel
@@ -31,16 +33,10 @@ class MainActivity : AppCompatActivity() {
         viewPager.addOnPageChangeListener(object : OnPageChangeListener {
             override fun onPageScrolled(i: Int, v: Float, i1: Int) {}
             override fun onPageSelected(i: Int) {
-                val token = (viewPager.adapter as SectionsPagerAdapter).getPageTitle(i)
-
-                Toast.makeText(this@MainActivity,
-                    "${(viewPager.adapter as SectionsPagerAdapter).getPageTitle(i)}",
-                    Toast.LENGTH_LONG
-                ).show()
-
-                pageViewModel.getPostResponse(Types(types = listOf(1)), token as String).observe(this@MainActivity, {
+                val token = (viewPager.adapter as SectionsPagerAdapter).getPageTitle(i).toString()
+                Log.i("TAT", "BLA")
+                pageViewModel.getPostResponse(Types(types = listOf(1)), "Bearer $token").observe(this@MainActivity, {
                     it?.let {
-                        Log.i("LAST", it.data.toString())
                         it.data?.let {
                             when (it.status) {
                                 Resource.Status.SUCCESS -> {
@@ -54,11 +50,17 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                 })
+                //Log.i("tes:", "${(viewPager.adapter as SectionsPagerAdapter).getPageTitle(i)}" )
             }
 
-            override fun onPageScrollStateChanged(i: Int) {}
+            override fun onPageScrollStateChanged(i: Int) {
+            }
         })
     }
+    private fun fetchData(token: String) {
+
+    }
+
     private fun setupUI() {
         pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java)
     }
